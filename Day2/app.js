68eaaa0f635d0e4b3dcb1019beae9240aafc5719
Tanmay Raj <tanmayraj_2k18se127@dtu.ac.in>
2022-02-17 00:16:35 +0530

    Day2 code

---
+++ Day2/app.js
@@ -0,0 +1,17 @@
+function changeTextColor() {
+    var x = document.getElementById("div1");
+    x.style.color = 'red';
+}
+function changeTextColor2() {
+    var x = document.getElementById("div2");
+    x.style.color = 'red';
+}
+
+function resetTextColor() {
+    var x = document.getElementById("div1");
+    x.style.color = 'black';
+}
+function resetTextColor2() {
+    var x = document.getElementById("div2");
+    x.style.color = 'black';
+}
